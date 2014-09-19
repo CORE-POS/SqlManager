5a2e082d9e306f71fade31600004f12bd0917ee7
Andy Theuninck <andy@gohanman.com>
2014-09-19 14:14:27 -0500

    Grab the autoloader for Composer if available and necessary

--- SqlManager.php
+++ SqlManager.php
@@ -1,6 +1,17 @@
 <?php
 
 /**
+  If Composer's autoloader is not active but is available
+  within the package directory, include it
+*/
+if (
+    !class_exists('\\Composer\\Autoload\\ClassLoader', false) &&
+    file_exists(dirname(__FILE__) . '/vendor/autoload.php')
+    ) {
+        include(dirname(__FILE__) . '/vendor/autoload.php');
+}
+
+/**
  @class SqlManager
  @brief A SQL abstraction layer
 
